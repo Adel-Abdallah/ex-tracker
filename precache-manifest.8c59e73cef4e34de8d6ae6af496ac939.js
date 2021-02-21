13d4557ce9ec1f857bac645d710584f7eeb42f0c
Adel-Abdallah <adelgafo@gmail.com>
2021-02-21 17:34:57 +0200

    Updates

---
+++ precache-manifest.8c59e73cef4e34de8d6ae6af496ac939.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d0674cd8dd626e749eb6e72da1f4e9",
+    "url": "/ex-tracker/index.html"
+  },
+  {
+    "revision": "b9c507f47113cb39fd04",
+    "url": "/ex-tracker/static/css/main.ba037b4b.chunk.css"
+  },
+  {
+    "revision": "4cc263c2a129bd502390",
+    "url": "/ex-tracker/static/js/2.d89c8a62.chunk.js"
+  },
+  {
+    "revision": "f5fc2afae58c4ffc30c6148864765d4b",
+    "url": "/ex-tracker/static/js/2.d89c8a62.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9c507f47113cb39fd04",
+    "url": "/ex-tracker/static/js/main.4f6c918c.chunk.js"
+  },
+  {
+    "revision": "ef43218b2c5327bd87d5",
+    "url": "/ex-tracker/static/js/runtime-main.0129fb58.js"
+  }
+]);
